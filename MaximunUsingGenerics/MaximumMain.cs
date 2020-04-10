28605842ab2ee9d2f366621a23a29ae1f923d45a
nitikeshs58 <nitikeshs58@gmail.com>
2020-04-10 16:58:11 +0530

    Added: Welcome message to Maximum problem using generics.

---
+++ MaximunUsingGenerics/MaximumMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaximunUsingGenerics
+{
+    class MaximumMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Maximum problem using generics!");
+        }
+    }
+}
