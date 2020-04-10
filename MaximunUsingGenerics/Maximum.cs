58bdfecf6adb452777995abe5fe08da1089faf24
nitikeshs58 <nitikeshs58@gmail.com>
2020-04-10 18:43:44 +0530

    Added: Maximum among 3 float values and checked test case passed.

--- MaximunUsingGenerics/Maximum.cs
+++ MaximunUsingGenerics/Maximum.cs
@@ -6,17 +6,13 @@ namespace MaximunUsingGenerics
 {
     public class Maximum
     {
-        int number1, number2, number3;
-
-        public Maximum(){}
-
-        public Maximum(int number1,int number2, int number3)
-        {
-            this.number1 = number1;
-            this.number1 = number2;
-            this.number3 = number3;
-        }
-
+        /// <IntergerMaximum>
+        /// method to find maximum of three integer numbers
+        /// </IntergerMaximum>
+        /// <param name="interger1"> number 1 </param>
+        /// <param name="interger2"> number 2 </param>
+        /// <param name="interger3"> number 3 </param>
+        /// <returns> maximum number </returns>
         public int IntergerMaximum(int number1,int number2,int number3)
         {
             int max = number1;
@@ -25,6 +21,27 @@ namespace MaximunUsingGenerics
                 max = number2;
             }
             if(max<number3)
+            {
+                max = number3;
+            }
+            return max;
+        }
+
+        /// <IntergerMaximum>
+        /// method to find maximum of three integer numbers
+        /// </IntergerMaximum>
+        /// <param name="floatValue1"> number 1 </param>
+        /// <param name="floatValue1"> number 2 </param>
+        /// <param name="floatValue1"> number 3 </param>
+        /// <returns> maximum number </returns>
+        public float floatMaximum(float number1, float number2, float number3)
+        {
+            float max = number1;
+            if (max < number2)
+            {
+                max = number2;
+            }
+            if (max < number3)
             {
                 max = number3;
             }
--- TestMaximum/TestMaximum.cs
+++ TestMaximum/TestMaximum.cs
@@ -10,8 +10,14 @@ namespace Tests
         {
         }
 
+        /// <IntegerTest>
+        /// taken 3 integer values
+        /// maxNumber as object of Maximum class
+        /// parameterised IntergerMaximum method is called
+        /// Compared returnMaximum value to expected vaule
+        /// </IntegerTest>
         [Test]
-        public void Test1()
+        public void IntegerTest()
         {
             int interger1 = 5,
                 interger2 = 2,
@@ -19,6 +25,23 @@ namespace Tests
             Maximum maxNumber = new Maximum();
             int returnMaximum=maxNumber.IntergerMaximum(interger1, interger2, interger3);
             Assert.Pass("9", returnMaximum);
+        }
+
+        /// <FloatTest>
+        /// taken 3 float values
+        /// maxNumber as object of Maximum class
+        /// parameterised floatMaximum method is called
+        /// Compared returnMaximum value to expected vaule
+        /// </FloatTest>
+        [Test]
+        public void FloatTest()
+        {
+            float floatValue1 = 5.2F,
+                floatValue2 = 2.34F,
+                floatValue3 = 9.65F;
+            Maximum maxNumber = new Maximum();
+            float returnMaximum = maxNumber.floatMaximum(floatValue1, floatValue2, floatValue3);
+            Assert.Pass("9.65F", returnMaximum);
         }
     }
 }
